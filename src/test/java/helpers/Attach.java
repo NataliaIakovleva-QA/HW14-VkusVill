0418938c9ecfbc9ce5a7e566e8d569583a44aee6
Natalia Iakovleva <natalia_yakovleva@yahoo.com>
2025-04-23 21:01:44 +0300

    video attachment modified

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -38,20 +38,8 @@ public class Attach {
 
     @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
     public static String addVideo() {
-        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
-                + getVideoUrl()
-                + "' type='video/mp4'></video></body></html>";
-    }
-
-    public static URL getVideoUrl() {
-        String remoteWebDriver = System.getProperty("remoteWebDriver", "selenoid.autotests.cloud");
-        String videoUrl = "https://" + remoteWebDriver + "/video/selenoid" + sessionId() + ".mp4";
-        try {
-            return new URL(videoUrl);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return "<html><body><video width='100%' height='100%' controls autoplay>" +
+                "<source src='https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4'></video></body></html>";
     }
 
 }
